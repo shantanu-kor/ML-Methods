d936a5f0bac39b6d12edc893042ee367f518e439
Shantanu Nitin Kor <77278554+s-h-a-n-k@users.noreply.github.com>
2022-09-13 03:03:08 +0530

    Update solnfn.py

--- ml_methods/algorithms/solnfn.py
+++ ml_methods/algorithms/solnfn.py
@@ -1,6 +1,6 @@
-import os, sys
+# import os, sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import math
 from functions import funcutils
